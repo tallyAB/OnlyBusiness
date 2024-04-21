42875d69734a32036b1304594aee201e0c926d51
Zain Ali Khokhar <zainalikhokhar40@gmail.com>
2024-04-21 19:03:41 +0500

    api code

---
+++ server/api.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import microservices as ms
+
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "Welcome to the AI Model API!"    
+
+
+@app.route('/get_response', methods=['POST'])
+def get_response_route():
+    print("here")
+    data = request.get_json()
+    response = ms.get_response(data)
+    return jsonify(response)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
