26384744ce32a803998b176092260de721dd2bbb
tallyAB <115516719+tallyAB@users.noreply.github.com>
2024-04-27 05:02:04 +0500

    Add thread functionality

--- bot/bot.py
+++ bot/bot.py
@@ -1,89 +1,121 @@
 from openai import OpenAI
+# import openai
 import time
 import json
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-import datetime
+from datetime import date
+import pandas as pd
+
 
 # import API_RowAppend file
 
+def insert_thread_id(whatsapp_num, thread_id):
+    print(whatsapp_num, thread_id)
+    whatsapp_num = '\'' + whatsapp_num
+    values = [[whatsapp_num, thread_id]]
+
+    SERVICE_ACCOUNT_FILE = r'C:\Users\Talha Abrar\Desktop\LUMS\SENIOR\Spring 2024\GEN AI\Project\OnlyBusiness\SpreadsheetAPI\onlybusinessdummy-8706fb48751e.json'
+    SPREADSHEET_ID = '1E_TLxnvSQgz2E7Y-5kFLJZtf8OogxPklmCQ819ip-vA'
+    RANGE_NAME = 'Sheet2'
+
+    # Authenticate and build the service
+    credentials = Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
+    service = build('sheets', 'v4', credentials=credentials)
+
+    # Call the Sheets API to append the data
+    request = service.spreadsheets().values().append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME,
+        valueInputOption='USER_ENTERED',
+        insertDataOption='INSERT_ROWS',
+        body={'values': values}
+    )
+    response = request.execute()
+
+    return
+
 
 class Bot:
-    def __init__(self,whatsapp_number, api_key = "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", assistant_id = "asst_8FWoRndfw1BUlalAHW0Xib45", thread_old = None , run_old = None):
+    def __init__(self, whatsapp_number, api_key="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", assistant_id="asst_8FWoRndfw1BUlalAHW0Xib45", thread_old=None, run_old=None):
         self.client = OpenAI(api_key=api_key)
         self.assistant_id = assistant_id
         self.number = whatsapp_number
         if thread_old is None:
             self.thread = self.client.beta.threads.create()
+            insert_thread_id(self.number, self.thread.id)
         else:
-            self.thread = self.client.beta.threads.retrieve(thread_id = thread_old)
-        
+            self.thread = self.client.beta.threads.retrieve(
+                thread_id=thread_old)
+
         self.data = self.get_spreadsheet_data()
 
+        self.assistant = self.client.beta.assistants.retrieve(
+            assistant_id=self.assistant_id)
+        # contains tuple of (query response) in ascending order (latest response at end of list)
+        self.history = []
 
-        self.assistant = self.client.beta.assistants.retrieve(assistant_id = self.assistant_id)
-        self.history = [] # contains tuple of (query response) in ascending order (latest response at end of list)
     def print_history(self):
-        for msg , rsp in self.history:
+        for msg, rsp in self.history:
             print(f"User: {msg}\nBot: {rsp}\n")
 
     def get_spreadsheet_data(self):
 
-            SERVICE_ACCOUNT_FILE = r'onlybusinessdummy-8706fb48751e.json' 
-            SPREADSHEET_ID = '1E_TLxnvSQgz2E7Y-5kFLJZtf8OogxPklmCQ819ip-vA'
-            RANGE_NAME = 'Sheet1'
+        SERVICE_ACCOUNT_FILE = r'C:\Users\Talha Abrar\Desktop\LUMS\SENIOR\Spring 2024\GEN AI\Project\OnlyBusiness\SpreadsheetAPI\onlybusinessdummy-8706fb48751e.json'
+        SPREADSHEET_ID = '1E_TLxnvSQgz2E7Y-5kFLJZtf8OogxPklmCQ819ip-vA'
+        RANGE_NAME = 'Sheet1'
 
-            # Authenticate and build the service
-            credentials = Credentials.from_service_account_file(
-                    SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
-            service = build('sheets', 'v4', credentials=credentials)
+        # Authenticate and build the service
+        credentials = Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
+        service = build('sheets', 'v4', credentials=credentials)
 
-            # Call the Sheets API to append the data
-            sheet = service.spreadsheets().values().get(
+        # Call the Sheets API to append the data
+        sheet = service.spreadsheets().values().get(
             spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME, majorDimension='ROWS').execute()
-            
-            sheet_values = sheet['values']
-            column_names = sheet_values[0]
-            data_rows = sheet_values[1:]
-            
-            df = pd.DataFrame(data_rows, columns=column_names)
-            today_date = str(date.today())
-            today_date = today_date.split('-')
-            today_date.reverse()
-            today_date = "-".join(today_date)
-            query_results = df[df['Order Date'] == today_date]
-            query_results = [query_results.columns.tolist()] + query_results.values.tolist()
-            return query_results
-    
 
+        sheet_values = sheet['values']
+        column_names = sheet_values[0]
+        data_rows = sheet_values[1:]
 
-    def wait_on_run(self,run, thread):
-        
+        df = pd.DataFrame(data_rows, columns=column_names)
+        today_date = str(date.today())
+        today_date = today_date.split('-')
+        today_date.reverse()
+        today_date = "-".join(today_date)
+        query_results = df[df['Order Date'] == today_date]
+        query_results = [query_results.columns.tolist()] + \
+            query_results.values.tolist()
+        return query_results
+
+    def wait_on_run(self, run, thread):
+
         while run.status == "queued" or run.status == "in_progress":
             run = self.client.beta.threads.runs.retrieve(
                 thread_id=thread.id,
                 run_id=run.id,
             )
             time.sleep(0.5)
-        
+
         return run
-    
-    def insert_data_to_spreadsheet(self,values = None):
-    # schema : [order_id, customer_id, customer_name, order date, order items, status, rider, Rider contact number, Delivery address, Amount, Rating]
 
-    # values should be passed in the following format:
+    def insert_data_to_spreadsheet(self, values=None):
+        # schema : [order_id, customer_id, customer_name, order date, order items, status, rider, Rider contact number, Delivery address, Amount, Rating]
 
-        #values = [["23", "69", "Zain Ali Khokhar", "01-03-2024", "HP Envy Screen Protector, HP Envy Hinge", "Delivered", "Sponge-Bob", "03248433434", "Out of Lahore", "$6666.44", "9", "Added through API"]]
+        # values should be passed in the following format:
+
+        # values = [["23", "69", "Zain Ali Khokhar", "01-03-2024", "HP Envy Screen Protector, HP Envy Hinge", "Delivered", "Sponge-Bob", "03248433434", "Out of Lahore", "$6666.44", "9", "Added through API"]]
         values[0][1] = self.number
 
-        SERVICE_ACCOUNT_FILE = r'C:\Users\Ahad Imran\Desktop\GenAI\Project\onlybusinessdummy-8706fb48751e.json' 
+        # SERVICE_ACCOUNT_FILE = r'C:\Users\Ahad Imran\Desktop\GenAI\Project\onlybusinessdummy-8706fb48751e.json'
+        SERVICE_ACCOUNT_FILE = r'C:\Users\Talha Abrar\Desktop\LUMS\SENIOR\Spring 2024\GEN AI\Project\OnlyBusiness\SpreadsheetAPI\onlybusinessdummy-8706fb48751e.json'
         SPREADSHEET_ID = '1E_TLxnvSQgz2E7Y-5kFLJZtf8OogxPklmCQ819ip-vA'
         RANGE_NAME = 'Sheet1'
 
-
         # Authenticate and build the service
         credentials = Credentials.from_service_account_file(
-                SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
+            SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
         service = build('sheets', 'v4', credentials=credentials)
 
         # Call the Sheets API to append the data
@@ -101,22 +133,20 @@ class Bot:
     def call_required_function(self, tools_called):
 
         for tool in tools_called:
-            
+
             if (tool.function.name == 'insert_data_to_spreadsheet'):
                 values_param = json.loads(tool.function.arguments)['values']
                 response = self.insert_data_to_spreadsheet(values_param)
 
         return response
-    
-
 
     def send_message(self, message_content):
         """
         Sends a message to the bot and waits for the response.
-        
+
         Args:
         message_content (str): The content of the message the user sends to the bot.
-        
+
         Returns:
         str: The bot's response.
         """
@@ -124,22 +154,23 @@ class Bot:
         message = self.client.beta.threads.messages.create(
             thread_id=self.thread.id,
             role="user",
-            content= message_content,
+            content=message_content,
         )
         run = self.client.beta.threads.runs.create(
             thread_id=self.thread.id,
             assistant_id=self.assistant.id,
-            )
+        )
         run = self.wait_on_run(run, self.thread)
-        
+
         print(run.status)
         if (run.status == 'requires_action'):
-            check = self.call_required_function(run.required_action.submit_tool_outputs.tool_calls)
+            check = self.call_required_function(
+                run.required_action.submit_tool_outputs.tool_calls)
             print(check)
 
-        messages = self.client.beta.threads.messages.list(thread_id=self.thread.id)
-       
+        messages = self.client.beta.threads.messages.list(
+            thread_id=self.thread.id)
+
         response = messages.to_dict()["data"][0]["content"][0]['text']['value']
         self.history.append((message_content, response))
         return response
-
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 # Third-party imports
-import openai
+# import openai
 from fastapi import FastAPI, Form, Depends, Request
 from decouple import config
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
-from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
 
 # Internal imports
 from models import Conversation, SessionLocal
 from utils import send_message, logger
 from bot.bot import Bot
 
+# Sheets imports
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+import pandas as pd
+
 
 app = FastAPI()
 # Set up the OpenAI API client
-openai.api_key = config("OPENAI_API_KEY")
-
-# Dependency
+# openai.api_key = config("OPENAI_API_KEY")
 
 
 def get_db():
@@ -39,12 +40,13 @@ async def reply(request: Request, Body: str = Form(), db: Session = Depends(get_
 
     form_data = await request.form()
     whatsapp_number = form_data['From'].split("whatsapp:")[-1]
-    print(f"Sending the ChatGPT response to this number: {whatsapp_number}")
+
+    thread_id = fetch_thread(whatsapp_number)
 
     # Call the OpenAI API to generate text with ChatGPT
 
-    new_bot = Bot(thread_old='thread_YColdX7bNaptVoggzz58TMYu', whatsapp_number = whatsapp_number)
-    
+    new_bot = Bot(thread_old=thread_id,
+                  whatsapp_number=whatsapp_number)
 
     messages = [{"role": "user", "content": Body}]
     messages.append(
@@ -60,6 +62,8 @@ async def reply(request: Request, Body: str = Form(), db: Session = Depends(get_
 
     # # The generated text
     # chatgpt_response = response.choices[0].message.content
+
+    # The generated text
     chatgpt_response = new_bot.send_message(Body)
 
     # Store the conversation in the database
@@ -75,8 +79,37 @@ async def reply(request: Request, Body: str = Form(), db: Session = Depends(get_
     except SQLAlchemyError as e:
         db.rollback()
         logger.error(f"Error storing conversation in database: {e}")
+
+    print(f"Sending the ChatGPT response to this number: {whatsapp_number}")
     send_message(whatsapp_number, chatgpt_response)
     return ""
 
 
-get_prev_thread()
+def fetch_thread(whatsapp_num):
+    # schema : [order_id, customer_id, customer_name, order date, order items, status, rider, Rider contact number, Delivery address, Amount, Rating]
+
+    SERVICE_ACCOUNT_FILE = r'C:\Users\Talha Abrar\Desktop\LUMS\SENIOR\Spring 2024\GEN AI\Project\OnlyBusiness\SpreadsheetAPI\onlybusinessdummy-8706fb48751e.json'
+    SPREADSHEET_ID = '1E_TLxnvSQgz2E7Y-5kFLJZtf8OogxPklmCQ819ip-vA'
+    RANGE_NAME = 'Sheet2'
+
+    # Authenticate and build the service
+    credentials = Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=['https://www.googleapis.com/auth/spreadsheets'])
+    service = build('sheets', 'v4', credentials=credentials)
+
+    # Call the Sheets API to append the data
+    sheet = service.spreadsheets().values().get(
+        spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME, majorDimension='ROWS').execute()
+
+    sheet_values = sheet['values']
+    column_names = sheet_values[0]
+    data_rows = sheet_values[1:]
+
+    df = pd.DataFrame(data_rows, columns=column_names)
+    query_results = df[df['Phone Number'] == whatsapp_num]
+    print("CHECK\n", df['Phone Number'], "\n", whatsapp_num)
+    if len(query_results) > 0:
+        print("LAME", query_results['Thread ID'].values[0])
+        return query_results['Thread ID'].values[0]
+    else:
+        return None
